347fdd295b0e56df2e873b80f41d35d8cbdf9df0
jim0409 <berserker.01.tw@hotmail.com>
2020-04-26 16:05:13 +0800

    arangodb cluster

--- arangodb-cluster/agency1.sh
+++ arangodb-cluster/agency1.sh
@@ -1,6 +1,7 @@
 arangod --server.endpoint tcp://0.0.0.0:8531 \
   --server.authentication false \
-  --agency.my-address tcp://192.168.51.119:8531 \
+  --server.export-metrics-api true \
+  --agency.my-address tcp://192.168.51.188:8531 \
   --agency.activate true \
   --agency.size 3 \
   --agency.supervision true \
