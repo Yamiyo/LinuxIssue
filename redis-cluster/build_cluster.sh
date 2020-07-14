e1d8061260c61f71c290d53019cbbcc5444511a8
jim0409 <berserker.01.tw@hotmail.com>
2020-07-15 01:30:49 +0800

    update redis-lua-script

--- redis-cluster/build_cluster.sh
+++ redis-cluster/build_cluster.sh
@@ -2,5 +2,6 @@
 REDIS_CLUSTER_IP=`docker network ls|grep 'redis-cluster'|awk '{print $1}'|xargs docker network inspect|grep Gateway|awk '{print $2}'|tr -d '"'`
 
 # password less
-docker exec -it redis1 redis-cli -c -p 7001 --cluster create $REDIS_CLUSTER_IP:7001 $REDIS_CLUSTER_IP:7002 $REDIS_CLUSTER_IP:7003 $REDIS_CLUSTER_IP:7004 $REDIS_CLUSTER_IP:7005 $REDIS_CLUSTER_IP:7006 --cluster-replicas 1
+# docker exec -it redis1 redis-cli -c -p 7001 --cluster create $REDIS_CLUSTER_IP:7001 $REDIS_CLUSTER_IP:7002 $REDIS_CLUSTER_IP:7003 $REDIS_CLUSTER_IP:7004 $REDIS_CLUSTER_IP:7005 $REDIS_CLUSTER_IP:7006 --cluster-replicas 1
+docker exec -it redis1 sh -c "echo 'yes' | redis-cli -c -p 7001 --cluster create $REDIS_CLUSTER_IP:7001 $REDIS_CLUSTER_IP:7002 $REDIS_CLUSTER_IP:7003 $REDIS_CLUSTER_IP:7004 $REDIS_CLUSTER_IP:7005 $REDIS_CLUSTER_IP:7006 --cluster-replicas 1"
 
